23c76eba55ce642d38522dd60545b21bbef930d2
Hendrik Fick Renaldo Potgieter (Henro) <53427025+henro47@users.noreply.github.com>
2023-12-17 17:57:45 +0200

    Boostrap compent proven

---
+++ src/components/ui/ImageCard.js
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import classes from './ImageCard.module.css'
+
+function ImageCard(props) {
+  return (
+    <Card className={classes.card} > 
+        <Card.Img variant={props.imgPos} src={props.imgUrl} />
+          <Card.Body>
+        <Card.Title>{props.Title}</Card.Title>
+        <Card.Text>
+          {props.Description}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ImageCard;
