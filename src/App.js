4bfaf986d8aaf623c52e0f7302319443e4b4197d
Hendrik Fick Renaldo Potgieter (Henro) <53427025+henro47@users.noreply.github.com>
2023-12-18 21:00:50 +0200

    rm use state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { useState } from 'react';
 import './App.css';
 import Layout from './components/layout/Layout'
 function App() {
-  const [loading, setLoading] = useState(true);
   return (
     <Layout></Layout>
   )
