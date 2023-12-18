264219ff26e300d6417bb8f88c8c49237b34bcc8
Hendrik Fick Renaldo Potgieter (Henro) <53427025+henro47@users.noreply.github.com>
2023-12-18 15:28:56 +0200

    Set timeline in card

--- src/components/ui/Timeline.js
+++ src/components/ui/Timeline.js
@@ -1,24 +1,27 @@
 import classes from './Timeline.module.css';
+import Card from 'react-bootstrap/Card';
 
 function Timeline(props) {
     console.log(props)
     return (
-        <div className={classes.container}>
-            <div className={classes.wrapper}>
-                <h4>{props.Timeline.Title}</h4>
-                <ul>
-                    {props.Timeline.Dates.map((item, i) => {
-                    return (
-                        <li key={i}>
-                            <div className={classes.time}>{item.Date}</div>
-                            <p>{item.Title}</p>
-                            <p>{item.Description}</p>
-                        </li>
-                      );
-                    })}
-                </ul>
-            </div>
-        </div>
+        <Card className={classes.card}>
+            <Card.Body>
+                <Card.Title>{props.Timeline.Title}</Card.Title>
+                <Card.Text>
+                    <ul>
+                        {props.Timeline.Dates.map((item, i) => {
+                        return (
+                            <li key={i}>
+                                <div className={classes.time}>{item.Date}</div>
+                                <p>{item.Title}</p>
+                                <p>{item.Description}</p>
+                            </li>
+                        );
+                        })}
+                    </ul>   
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
